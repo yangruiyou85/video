7671502dec5544956a73a2510364838dfe04105b
ruiyou.yang <ruiyou.yang@gmail.com>
2018-07-10 23:51:36 +0800

    api

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,15 +1,55 @@
 package dbops
 
-import "database/sql"
+import "log"
 
-func OpenConn() *sql.DB {
-
-}
+//func OpenConn() *sql.DB {
+//
+//	dbConn, err := sql.Open("mysql", "root:mysql@tcp(localhost:3306)/video?charset=utf8")
+//	if err != nil {
+//		panic(err.Error())
+//
+//	}
+//	return dbConn
+//
+//
+//}
 
 func AddUserCredential(loginName string, pwd string) error {
 
+	stmtIns, err := dbConn.Prepare("insert into users(login_name,pwd)values(?,?)")
+	if err != nil {
+		return err
+
+	}
+
+	stmtIns.Exec(loginName, pwd)
+	stmtIns.Close()
+	return nil
 }
 
 func GetUserCredential(loginName string) (string, error) {
 
+	stmtOut, err := dbConn.Prepare("select pwd from users where login_name=?")
+	if err != nil {
+		log.Printf("%s", err)
+		return "", err
+	}
+	var pwd string
+	stmtOut.QueryRow(loginName).Scan(&pwd)
+
+	stmtOut.Close()
+	return pwd, nil
+
+}
+
+func DeleteUser(loginName string, pwd string) error {
+	stmtDel, err := dbConn.Prepare("delete from users where login_name=? and  pwd=?")
+	if err != nil {
+		log.Printf("DeleteUser error:%s", err)
+		return err
+	}
+
+	stmtDel.Exec(loginName, pwd)
+	stmtDel.Close()
+	return err
 }
---
+++ sql/db.sql
@@ -0,0 +1,26 @@
+
+create database video default charset utf8;
+use video;
+
+
+create table users(author_id bigint not null primary key auto_increment,
+login_name varchar(64) ,pwd text) engine=innodb;
+
+
+create table video_info(video_id varchar(64) not null primary key,author_id bigint,name text,
+display_ctime text,create_time datetime) engine=innodb;
+
+
+
+create table sessions(session_id tinytext  not null ,
+ttl tinytext,login_name varchar(64)) engine=innodb;
+
+
+
+create table  comments(command_id varchar(64) not null primary key,video_id varchar(64),
+
+author_id  bigint,content text,time datetime
+) engine=innodb;
+
+
+
